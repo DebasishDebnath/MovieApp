4b0abffd75c4a7e442e4b7be63f1f655eb4784d8
Debasish Debnath <ddebnath241@gmail.com>
2025-05-14 10:33:34 +0530

    feat: implement tab navigation and update app branding

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,12 @@
+import { Link } from "expo-router";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-red-100">
-      <Text className="text-3xl font-bold text-accent">Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl font-bold text-accent">
+        Edit app/index.tsx to edit this screen.
+      </Text>
     </View>
   );
 }
